e033fb681af1862f40e6fe2332f2623e128b2ed3
zhu6 <zhu6@ncsu.edu>
2016-11-15 15:47:19 -0500

    Update

--- Deployment/infrastructure.js
+++ Deployment/infrastructure.js
@@ -1,44 +1,18 @@
 var redis   = require('redis');
-var http      = require('http');
+var http  = require('http');
 var httpProxy = require('http-proxy');
-var exec = require('child_process').exec;
+
 // REDIS
 var client = redis.createClient(6379, 'redis', {});
 
-var infrastructure =
-{
-  setup: function()
-  {
-    // Proxy.
-    var options = {};
-    var proxy   = httpProxy.createProxyServer(options);
-    var server  = http.createServer(function(req, res)
-    {
-      client.spop("serverSet", function(err, serverPort){
-        console.log("Current proxy server is http://0.0.0.0:" + serverPort);
-        proxy.web(req, res, {target: app:serverPort});
-        client.sadd("serverSet", serverPort);
-      });
-    });
-
-    server.listen(8080);
-  },
-
-  teardown: function()
-  {
-    exec('forever stopall', function()
-    {
-      console.log("infrastructure shutdown");
-      process.exit();
-    });
-  },
-}
-
-infrastructure.setup();
-
-// Make sure to clean up.
-process.on('exit', function(){infrastructure.teardown();} );
-process.on('SIGINT', function(){infrastructure.teardown();} );
-process.on('uncaughtException', function(err){
-  console.error(err);
-  infrastructure.teardown();} );
+// HTTP Proxy
+var proxy = httpProxy.createProxyServer({});
+var proxyServer = http.createServer(function(req, res){
+  client.spop("serverSet", function(err, serverPort){
+    console.log("Current proxy server is http://0.0.0.0:" + serverPort);
+    proxy.web(req, res, {target: {host: 'app', port: serverPort}});
+    client.sadd("serverSet", serverPort);
+  });
+});
+proxyServer.listen(8080);
+console.log("The proxy server delivers requests to http://%s:%s", proxyServer.address().address, proxyServer.address().port);
