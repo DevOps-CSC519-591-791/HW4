1c6e4cd212b8dbcfc34a0562e76c7b4691d422f2
zhu6 <zhu6@ncsu.edu>
2016-11-16 02:18:17 -0500

    Finish container_creator.sh script

--- Proxy/proxy.js
+++ Proxy/proxy.js
@@ -9,8 +9,10 @@ var client = redis.createClient(6379, 'redis', {});
 var proxy = httpProxy.createProxyServer({});
 var proxyServer = http.createServer(function(req, res){
   client.spop("serverSet", function(err, serverPort){
-    console.log("Current proxy server is http://0.0.0.0:" + serverPort);
-    proxy.web(req, res, {target: "http://app:" + serverPort});
+    // get the last character of port
+    var appNum = parseInt(serverPort.substr(-1)) + 1;
+    console.log("Current proxy server is app_" + appNum + " (http://0.0.0.0:" + serverPort + ")");
+    proxy.web(req, res, {target: "http://hw4_app_" + appNum + ":3000"});
     client.sadd("serverSet", serverPort);
   });
 });
--- container-creator.sh
+++ container-creator.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 # access a runtime redis
 #docker-compose run redis redis-cli -h redis
+
+# echo 'Add port to redis'
+# redis-bash-cli -h 172.18.0.1 SADD serverSet $1
+# echo 'Start the container'
+# docker run -d -p $1:3000 --link hw4_redis_1:redis --network=hw4_default hw4_app
+
+# obtain redis container ip
+REDIS_IP=`docker inspect --format '{{ .NetworkSettings.Networks.hw4_default.IPAddress }}' $(docker ps -aqf "name=redis")`
+
+echo 'Count current number of ips'
+NUM_OF_IP=$1
+
+# generate new port
+NEW_PORT="300$NUM_OF_IP"
+echo "Generate new port $NEW_PORT"
+
+echo 'Scale one more `app` container'
+docker-compose scale app=$((NUM_OF_IP+1))
+
+echo 'Sleep 5 seconds'
+sleep 5
+
 echo 'Add port to redis'
-redis-bash-cli -h 172.18.0.1 SADD serverSet $1
-echo 'Start the container'
-docker run -d -p $1:3000 --link hw4_redis_1:redis --network=hw4_default hw4_app
+# redis-bash-cli -h $REDIS_IP SADD serverSet $NEW_PORT
+for i in `seq 1 $1`; do
+	redis-bash-cli -h $REDIS_IP SADD serverSet "300$i"
+done
